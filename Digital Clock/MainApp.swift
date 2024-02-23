09c8dde82126093ba03e315267270c71c54ea144
xcv58 <i@xcv58.com>
2024-02-22 22:14:10 -0500

    fix: hide the bottom bar

--- Digital Clock/MainApp.swift
+++ Digital Clock/MainApp.swift
@@ -6,12 +6,14 @@ struct MainApp: App {
     var body: some Scene {
         WindowGroup(id: "main") {
             ContentView().environmentObject(WindowView(id: ""))
+                .persistentSystemOverlays(.hidden)
         }
         .windowStyle(.automatic)
         .windowResizability(.contentSize)
         
         WindowGroup(for: WindowView.ID.self) {$windowID in
             ContentView().environmentObject(WindowView(id: windowID!))
+                .persistentSystemOverlays(.hidden)
         }
         .windowStyle(.automatic)
         .windowResizability(.contentSize)
